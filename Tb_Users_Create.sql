ee3f46adcc0e565592237dc1b2173bcfc439c668
Sanjiv Kumar <Sanjiv.enest@gmail.com>
2013-02-04 11:21:36 +0530

    test commit

---
+++ Tb_Users_Create.sql
@@ -0,0 +1,26 @@
+IF EXISTS
+(
+	SELECT * FROM sys.objects 
+	WHERE	object_id = OBJECT_ID(N'Tb_Users')
+	AND		type IN ('U')
+)
+    DROP TABLE Tb_Users;
+
+
+CREATE TABLE Tb_Users
+(
+	iTbUsersID			INT IDENTITY(1,1)		NOT NULL,
+	vUserName			VARCHAR(50)				NOT NULL,
+	vAuthVal			VARCHAR(50)				NOT NULL,
+	iTbUserTypesID		INT						NOT NULL,
+	iDeleteFlag			INT						NOT NULL	DEFAULT 0,
+	iApprovedFlag		INT						NOT NULL	DEFAULT 0,
+	dtCreatedOn			DATETIME				NOT NULL	DEFAULT GETDATE(),
+	iCreatedByUserID	INT						NULL,
+	dtUpdatedOn			DATETIME				NULL,
+	iUpdatedByUserID	INT						NULL,
+	CONSTRAINT PK_Tb_Users_UsersID
+		PRIMARY KEY (iTbUsersID),
+	CONSTRAINT FK_Users_iTbUserTypesID_UserTypes_iTbUserTypesID
+		FOREIGN KEY (iTbUserTypesID) REFERENCES Tb_UserTypes(iTbUserTypesID)
+);
